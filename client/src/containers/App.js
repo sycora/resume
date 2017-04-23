0c6460f1c401e52355561dc93cc6b24e7be8d957
Peter Willis <peter.willis@mits-tools.com>
2017-04-23 17:38:45 +0100

    remove logo

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Profile from './Profile';
 import SignInButton from '../components/SignInButton';
-import logo from './logo.svg';
 import {authLoader, logout} from '../actions/authLoader';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
@@ -23,7 +22,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Resumé</h2>
         </div>
         <p className="App-intro">
