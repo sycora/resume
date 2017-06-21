1177a08e1ef5baf511de89f6f02fdd4adc8d319a
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-06-21 18:22:14 -0400

    started on AppHeader design

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -3,10 +3,49 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 const styleMain = {
-    overflow: 'hidden'
+    height: 160,
+    overflow: 'hidden',
+    'background': 'rgba(41,52,106,1)',
+    'background': '-moz-linear-gradient(-23.6deg, rgba(41,52,106,1) 0%, rgba(41,52,106,1) 164px, rgba' +
+            '(41,52,105,0) 165px, rgba(44,42,92,0) 100%)',
+    /* FF3.6-15 */
+    'background': '-webkit-linear-gradient(-23.6deg, rgba(41,52,106,1) 0%,rgba(41,52,106,1) 164px,rgb' +
+            'a(41,52,105,0) 165px,rgba(44,42,92,0) 100%)',
+    /* Chrome10-25,Safari5.1-6 */
+    'background': 'linear-gradient(156.4deg, rgba(41,52,106,1) 0%,rgba(41,52,106,1) 164px,rgba(41,52,' +
+            '105,0) 165px,rgba(44,42,92,0) 100%)',
+    /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: `progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#002c2a5c',GradientType=1 )`/* IE6-9 fallback on horizontal gradient */
 };
 
-const styleContainer = {};
+const styleContainer = {
+    'background-color': '#3F435E',
+    'box-sizing': 'border-box',
+    width: '120%',
+    height: '100%',
+    '-webkit-transform-origin': '0 100%',
+    'transform-origin': '0 100%',
+    transform: 'rotate(-23.6deg) translate(49px, -1px)',
+    color: '#FFF',
+};
+
+const styleImage = {
+    background: '#AA3C2D',
+    'border-radius': '50%',
+    bottom: -20,
+    height: 60,
+    left: 160,
+    position: 'absolute',
+    width: 60
+}
+
+const styleTitle = {
+    color: 'white',
+    'font-family': `'Racing Sans One', cursive`,
+    'font-size': 36,
+    margin: '0 8px'
+
+}
 
 class AppHeader extends Component {
     componentWillReceiveProps(nextProps) {
@@ -16,13 +55,25 @@ class AppHeader extends Component {
                 .profileLoader();
         }
     }
-    
+
     render() {
         const {me, isFetching} = this.props;
         const profile = me.profile;
         return (
             <main style={styleMain}>
-                <div style={styleContainer}></div>
+                <div style={styleContainer}>
+                    <div style={{position:'absolute', bottom: 0, left: 0}}>
+                    <h3 style={styleTitle}>Résumé</h3>
+                    <img style={styleImage} src="" alt=""/>
+                    </div>
+                    
+                    {!profile
+                        ? (isFetching
+                            ? <h3>Getting profile...</h3>
+                            : <h3>RES</h3>)
+                        : <p>User</p>
+}
+                </div>
             </main>
         )
     }
