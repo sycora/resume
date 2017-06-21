08016b30bb25e70029e0b66a51a539806cc2c820
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-06-21 15:46:56 -0400

    added redux user to AppHeader

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,11 +1,39 @@
+import {profileLoader} from '../actions/profileLoader';
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+const styleMain = {
+    overflow: 'hidden'
+};
+
+const styleContainer = {};
 
 class AppHeader extends Component {
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.id) {
+            this
+                .props
+                .profileLoader();
+        }
+    }
+    
     render() {
+        const {me, isFetching} = this.props;
+        const profile = me.profile;
         return (
-            <div>APP HEADER</div>
+            <main style={styleMain}>
+                <div style={styleContainer}></div>
+            </main>
         )
     }
 }
 
-export default AppHeader;
+const reduxStateToProps = state => {
+    const {auth, me} = state;
+
+    const {isFetching} = me;
+
+    return {auth, me, isFetching}
+}
+
+export default connect(reduxStateToProps, {profileLoader})(AppHeader)
