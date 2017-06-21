6d2fd9c4a4f159c30480d028bb2614242791d978
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-06-21 19:57:36 -0400

    added react-tap-event-plugin
    into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {HashRouter, Route} from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import WebFont from 'webfontloader';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 const store = configureStore();
 
 const render = (App) =>
