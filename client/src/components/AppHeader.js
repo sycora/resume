9f1bab3ae598b3b0484372213fbd31c01a17091d
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-06-21 15:33:36 -0400

    started AppHeader component

---
+++ client/src/components/AppHeader.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <div>APP HEADER</div>
+        )
+    }
+}
+
+export default AppHeader;
