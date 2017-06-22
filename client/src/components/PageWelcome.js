1a7f4665c5c0e916ad91df988f6433d4592127b7
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-06-21 20:00:21 -0400

    Added content to PageWelcome

--- client/src/components/PageWelcome.js
+++ client/src/components/PageWelcome.js
@@ -1,4 +1,3 @@
-
 import SignInButton from '../components/SignInButton';
 import {authLoader, logout} from '../actions/authLoader';
 import React, {Component} from 'react';
@@ -26,9 +25,17 @@ class PageWelcome extends Component {
             <main style={styleMain}>
                 <div style={styleContainer}>
                     <p className="App-intro">
-                        {!isFetching && <SignInButton id={auth.id} signInClick={authLoader} signOutClick={logout}/>
-}
+                        Welcome to résumé.<br/>
+                        no more papér<br/>
+                        no more pain<br/>
+                        just you,<br/>
+                        your éxperiencé<br/>
+                        &<br/>
+                        the futuré.
+
                     </p>
+                    <h4>Begin<br/>with</h4>
+                    {!isFetching && <SignInButton id={auth.id} signInClick={authLoader} signOutClick={logout}/>}
                     {isEmpty
                         ? (isFetching
                             ? <h2>Logging in...</h2>
@@ -41,6 +48,12 @@ class PageWelcome extends Component {
                         }}></div>
 }
                 </div>
+                <div
+                    style={{
+                    backgroundColor: '#2C2A5C',
+                    height: 3,
+                    width: '100%'
+                }}/>
             </main>
         )
     }
