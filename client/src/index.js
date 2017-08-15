2513fb8bfb60013ea42a7d96cf2414f1e310111d
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2017-08-15 09:52:45 +0100

    added manifest and service worker script

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import App from './containers/App';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 import {configureStore} from './store';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -37,6 +38,7 @@ window.loaded = function() {
   }));
 
   render(App);
+  registerServiceWorker();
 
   WebFont.load({
   google: {
